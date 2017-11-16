179451f7c8bc715d885b0a2cf070d148b8583c4f
demianbacigalupo <mercenary666_12@hotmail.com>
2017-11-15 21:50:29 -0300

    Creado de Pedido Controlador, Servicio y DAO (ver Descripcion)
    En Controlador se crearon los siguientes metodos
    	public ModelAndView listaPedidosPendienteCobro()
    	public ModelAndView listaPedidosPendienteCoccion()
    	public ModelAndView listaPedidosPendienteEntrega()
    	public ModelAndView listaPedidosEntregados()
    En Servicio se crearon los siguientes metodos y su correspondiente
    implementacion
    	public Pedido consultarPedidoPorId(Long idPedido);
    	public void actualizarEstadoPedidoAPendCoccion(Long idPedido);
    	public void actualizarEstadoPedidoAPendEntrega(Long idPedido);
    	public void actualizarEstadoPedidoAEntregado(Long idPedido);
    	public List<Pedido> listarPedidosPendCobro();
    	public List<Pedido> listarPedidosPendCoccion();
    	public List<Pedido> listarPedidosPendEntrega();
    	public List<Pedido> listarPedidosEntregados();
    En DAO se crearon los siguientes metodos y su correspondiente
    implementacion
    	public Pedido consultarPedidoPorId(Long idPedido);
    	public Pedido actualizarEstadoPedidoAPendCoccion(Long idPedido);
    	public Pedido actualizarEstadoPedidoAPendEntrega(Long idPedido);
    	public Pedido actualizarEstadoPedidoAEntregado(Long idPedido);
    	public List<Pedido> listarPedidosPendCobro();
    	public List<Pedido> listarPedidosPendCoccion();
    	public List<Pedido> listarPedidosPendEntrega();
    	public List<Pedido> listarPedidosEntregados();

--- src/main/java/ar/edu/unlam/tallerweb1/dao/PedidoDao.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/PedidoDao.java
@@ -1,5 +1,21 @@
 package ar.edu.unlam.tallerweb1.dao;
 
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Pedido;
+
 public interface PedidoDao {
+	
+	public Pedido consultarPedidoPorId(Long idPedido);
+	
+	public Pedido actualizarEstadoPedidoAPendCoccion(Long idPedido);
+	public Pedido actualizarEstadoPedidoAPendEntrega(Long idPedido);
+	public Pedido actualizarEstadoPedidoAEntregado(Long idPedido);
+	
+	public List<Pedido> listarPedidosPendCobro();
+	public List<Pedido> listarPedidosPendCoccion();
+	public List<Pedido> listarPedidosPendEntrega();
+	public List<Pedido> listarPedidosEntregados();
+	
 
 }
--- src/main/java/ar/edu/unlam/tallerweb1/dao/PedidoDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/PedidoDaoImpl.java
@@ -1,22 +1,104 @@
 package ar.edu.unlam.tallerweb1.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import ar.edu.unlam.tallerweb1.modelo.Pan;
-
 import java.util.List;
 
 import javax.inject.Inject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unlam.tallerweb1.modelo.Pan;
+import ar.edu.unlam.tallerweb1.modelo.Pedido;
 
 @Repository("pedidoDao")
 public class PedidoDaoImpl implements PedidoDao {
 
 	@Inject
     private SessionFactory sessionFactory;
+
+	@Override
+	public Pedido consultarPedidoPorId(Long idPedido) {
+		try {
+			final Session session = sessionFactory.getCurrentSession();
+			Pedido pedido = (Pedido) session.createCriteria(Pedido.class)
+				.add(Restrictions.eq("idPedido", idPedido)).uniqueResult();
+			return pedido;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public Pedido actualizarEstadoPedidoAPendCoccion(Long idPedido) {
+		final Session session = sessionFactory.getCurrentSession();
+		Pedido pedido = (Pedido) session.createCriteria(Pedido.class)
+			.add(Restrictions.eq("idPedido", idPedido)).uniqueResult();
+		return pedido;
+		pedido.setEstado(2);
+		session.saveOrUpdate(pedido);
+	}
+
+	@Override
+	public Pedido actualizarEstadoPedidoAPendEntrega(Long idPedido) {
+		final Session session = sessionFactory.getCurrentSession();
+		Pedido pedido = (Pedido) session.createCriteria(Pedido.class)
+				.add(Restrictions.eq("idPedido", idPedido)).uniqueResult();
+		return pedido;
+		pedido.setEstado(3);
+		session.saveOrUpdate(pedido);
+	}
+
+	@Override
+	public Pedido actualizarEstadoPedidoAEntregado(Long idPedido) {
+		final Session session = sessionFactory.getCurrentSession();
+		Pedido pedido = (Pedido) session.createCriteria(Pedido.class)
+			.add(Restrictions.eq("idPedido", idPedido)).uniqueResult();
+		return pedido;
+		pedido.setEstado(4);
+		session.saveOrUpdate(pedido);
+	}
+
+	@Override
+	public List<Pedido> listarPedidosPendCobro() {
+		final Session session = sessionFactory.getCurrentSession();
+		@SuppressWarnings("unchecked")
+		List<Pedido> pedidos = session.createCriteria(Pan.class)
+				.add(Restrictions.eq("estado", 1))
+				.list();
+		return pedidos;
+	}
+
+	@Override
+	public List<Pedido> listarPedidosPendCoccion() {
+		final Session session = sessionFactory.getCurrentSession();
+		@SuppressWarnings("unchecked")
+		List<Pedido> pedidos = session.createCriteria(Pan.class)
+				.add(Restrictions.eq("estado", 2))
+				.list();
+		return pedidos;
+	}
+
+	@Override
+	public List<Pedido> listarPedidosPendEntrega() {
+		final Session session = sessionFactory.getCurrentSession();
+		@SuppressWarnings("unchecked")
+		List<Pedido> pedidos = session.createCriteria(Pan.class)
+				.add(Restrictions.eq("estado", 3))
+				.list();
+		return pedidos;
+	}
+
+	@Override
+	public List<Pedido> listarPedidosEntregados() {
+		final Session session = sessionFactory.getCurrentSession();
+		@SuppressWarnings("unchecked")
+		List<Pedido> pedidos = session.createCriteria(Pan.class)
+				.add(Restrictions.eq("estado", 4))
+				.list();
+		return pedidos;
+	}
 
 	
 }
