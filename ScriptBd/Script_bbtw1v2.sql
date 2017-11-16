2b54f2d9d1c1e676cdb6d1af18506e759c5d17e7
berto2904 <berto2904@gmail.com>
2017-11-16 16:39:54 -0300

    script de BD

---
+++ ScriptBd/Script_bbtw1v2.sql
@@ -0,0 +1,41 @@
+use unlamdb;
+
+delete from combo_ingrediente;
+delete from combo;
+
+select * from categoria;
+select * from combo;
+select * from combo_ingrediente;
+select * from ingrediente ;
+
+insert into categoria(descripcion)
+    values  ("Pan"),
+            ("Carne"),
+            ("Aderezo"),
+            ("Vegetales");
+
+insert into rol(nombre)
+    values  ("Administrador"),
+            ("Cliente"),
+            ("Cajero"),
+            ("Cocinero");
+            
+insert into usuario(nombre,email,password,idRol)
+values      ('pablo','prueba@gmail.com','1234',2);
+
+select * from usuario;
+
+insert into ingrediente(nombre, precio, tiempoCoccion, stock, idCategoria,activo)
+    values  ("Pan Blanco",20.00,null,30,1,1),
+            ("Pan Negro",20.00,null,30,1,1),
+            ("Pan De Campo",20.00,null,30,1,1),
+            ("Res",50.00,15,30,2,1),
+            ("Pollo",50.00,20,30,2,1),
+            ("Mayonesa",5.00,null,30,3,1),
+            ("Ketchup",5.00,null,30,3,1),
+            ("Mostaza",5.00,null,30,3,1),
+            ("Tomate",8.00,null,30,4,1),
+            ("Cebolla",8.00,null,30,4,1),
+            ("Pepino",8.00,null,30,4,1),
+            ("Lechuga",8.00,null,30,4,1);
+            
